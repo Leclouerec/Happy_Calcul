ce1596505c6fc97bbecc7c071d8ba82e9c7c34db
studlerobin <robin.Studle@yahoo.fr>
2017-02-27 14:42:23 +0100

    je sais aps

--- app/src/main/java/com/example/pierrick/happy_calcul/choix_jeux.java
+++ app/src/main/java/com/example/pierrick/happy_calcul/choix_jeux.java
@@ -2,6 +2,7 @@ package com.example.pierrick.happy_calcul;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,7 +15,51 @@ public class choix_jeux extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix_jeux);
+
+
+        final ImageView image = (ImageView) findViewById(R.id.imageView_chrono);
+        //int i = 0;
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            int i=0;
+            public void run() {
+                i++;
+                switch (i){
+                    case 1 :image.setImageResource(R.drawable.chrono_8);
+                        break;
+                    case 2 :image.setImageResource(R.drawable.chrono_7);
+                        break;
+                    case 3 :image.setImageResource(R.drawable.chrono_6);
+                        break;
+                    case 4 :image.setImageResource(R.drawable.chrono_5);
+                        break;
+                    case 5 :image.setImageResource(R.drawable.chrono_4);
+                        break;
+                    case 6 :image.setImageResource(R.drawable.chrono_3);
+                        break;
+                    case 7 :image.setImageResource(R.drawable.chrono_2);
+                        break;
+                    case 8 :image.setImageResource(R.drawable.chrono_1);
+                        break;
+                    case 9 :image.setImageResource(R.drawable.chrono_9);
+                        i=0;
+
+                        break;
+                    default : break;
+                }
+
+                handler.postDelayed(this, 1000);  //for interval...
+            }
+        };
+        handler.postDelayed(runnable, 1000); //for initial delay..
+
     }
+
+
+
+
+
+
 
     public void pageTables(View view){
         Intent intent = new Intent(choix_jeux.this, tables.class);
--- app/src/main/java/com/example/pierrick/happy_calcul/page_calcul.java
+++ app/src/main/java/com/example/pierrick/happy_calcul/page_calcul.java
@@ -1,16 +1,41 @@
 package com.example.pierrick.happy_calcul;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class page_calcul extends AppCompatActivity {
+
+    private int digit1 = 0;
+    private int digit2 = 0;
+    private int borneMin = 1;
+    private int borneMax = 10;
+    private int nombreDeQuestion = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_calcul);
+        lancerJeux();
+
+
+        final EditText resultat = (EditText) findViewById(R.id.editTextResultat);
+        resultat.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    Log.e("TEST", "Enter pressed :" + resultat.getText().toString());
+                }
+                return false;
+            }
+        });
+
     }
 
 
@@ -27,4 +52,48 @@ public class page_calcul extends AppCompatActivity {
         button b = new button();
         b.home(view, this);
     }
+
+    public int nombreAleatoire(int borneMin, int borneMax){
+
+        return (int)(Math.random()*(borneMax-borneMin));
+    }
+
+    public void lancerJeux(){
+
+        TextView digitGauche = (TextView) findViewById(R.id.textViewDigitGauche);
+        TextView digitDroite = (TextView) findViewById(R.id.textViewDigitDroite);
+        TextView numeroQuestion = (TextView) findViewById(R.id.textViewNombreQuestion);
+        TextView nbQuestion = (TextView) findViewById(R.id.textViewNbQuestion);
+        final EditText resultat = (EditText) findViewById(R.id.editTextResultat);
+
+        //final boolean boutonPressed = false;
+
+        nbQuestion.setText(" / " + String.valueOf(nombreDeQuestion));
+
+        for(int i = 1; i <= nombreDeQuestion ; i ++ ){
+            digit1 = nombreAleatoire(borneMin, borneMax);
+            digit2 = nombreAleatoire(borneMin, borneMax);
+
+
+
+            resultat.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                    if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                        Log.e("TEST", "Enter pressed :" + resultat.getText().toString());
+                    }
+                    return false;
+                }
+            });
+
+
+            digitGauche.setText(String.valueOf(digit1));
+            digitDroite.setText(String.valueOf(digit2));
+            numeroQuestion.setText(String.valueOf(i));
+
+        }
+
+
+    }
 }
+
